27f3470b6b9942190a86e494b8bb1d7c8ac59747
Matthew Hamilton <diogeneshamilton@gmail.com>
2014-02-22 16:44:54 -0500

    Updates for v1

--- main.rb
+++ main.rb
@@ -12,7 +12,7 @@ get '/' do
     if session['venmo_token']
         'Your Venmo token is %s' % session[:venmo_token]
     else
-        redirect to('https://api.venmo.com/oauth/authorize?client_id=%s&scope=make_payments,access_profile&response_type=code' % CONSUMER_ID)
+        redirect to('https://api.venmo.com/v1/oauth/authorize?client_id=%s&scope=make_payments,access_profile&response_type=code' % CONSUMER_ID)
     end
 end
 
@@ -23,8 +23,8 @@ get '/oauth-authorized' do
         "client_secret"=>CONSUMER_SECRET,
         "code"=>AUTHORIZATION_CODE
         }
-    url = "https://api.venmo.com/oauth/access_token"
-    response = RestClient.post 'https://api.venmo.com/oauth/access_token', data
+    url = "https://api.venmo.com/v1/oauth/access_token"
+    response = RestClient.post url, data
     response_dict = JSON.parse(response.to_str)
 
     access_token = response_dict['access_token']
